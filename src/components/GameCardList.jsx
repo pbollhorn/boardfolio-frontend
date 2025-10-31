800909f4e33276cc50661d0b50f8fccbabe52f7a
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-31 08:55:48 +0100

    cleanup

--- src/components/GameCardList.jsx
+++ src/components/GameCardList.jsx
@@ -4,7 +4,6 @@ export default function GameCardList({ list }) {
   return (
     <>
       {list.map((e) => (
-        // <p key={e.id}>{e.title}</p>
         <GameCard key={e.id} gameData={e} />
       ))}
     </>
