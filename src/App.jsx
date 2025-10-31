a678d3fbe3101a3d66f82005dd56c0b1b26ff029
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-31 08:04:39 +0100

    created BrowseGames component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import GameCardList from "./components/GameCardList.jsx";
+import BrowseGames from "./components/BrowseGames.jsx";
 import "./App.css";
 
 export default function App() {
@@ -9,8 +9,7 @@ export default function App() {
 
   return (
     <>
-      <p>Hello from App.jsx</p>
-      <GameCardList />
+      <BrowseGames />
     </>
   );
 }
---
+++ src/components/BrowseGames.jsx
@@ -0,0 +1,10 @@
+import GameCardList from "./GameCardList.jsx";
+
+export default function BrowseGames() {
+  return (
+    <>
+      <h1>Browse Games</h1>
+      <GameCardList />
+    </>
+  );
+}
