e1a7c0bdcfef4970275d0624883a7b3c16d3313b
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-03 13:33:07 +0100

    added Bootstraps classes to GameListItem.jsx

--- src/components/GameListItem.jsx
+++ src/components/GameListItem.jsx
@@ -2,16 +2,16 @@ import NoPoster from "../assets/NoPoster.png";
 
 export default function GameListItem({ gameData }) {
   return (
-    <div>
+    <div className="d-flex align-items-center mb-3 p-2 border rounded">
       <img
-        //   className={styles.posterImage}
+        className="me-3"
         src={
           gameData.posterPath
             ? "https://image.tmdb.org/t/p/w154" + gameData.posterPath
             : NoPoster
         }
       />
-      {" " + gameData.title}
+      <span className="fs-4 fw-semibold">{gameData.title}</span>
     </div>
   );
 }
