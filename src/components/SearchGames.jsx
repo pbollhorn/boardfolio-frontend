da01202f829d359eff18b73a3056df0a95565770
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-10 13:46:50 +0100

    Backup

--- src/components/SearchGames.jsx
+++ src/components/SearchGames.jsx
@@ -1,13 +1,18 @@
-import styles from "../styles/SearchGames.module.css"
+import styles from "../styles/SearchGames.module.css";
+
 export default function SearchGames() {
-return(
-    
- <div class="container">
-  <h1>Search for games</h1>
-  <input type="text" placeholder="Search for games" class="search-input" />
-  <button class="search-button">
-    <span class="material-symbols-outlined">search</span>
-  </button>
-</div>
-);
-} 
+  return (
+    <>
+      <div className="container">
+       
+        <div className="search-container">
+            <h1>Search For Games</h1>
+          <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search" />
+            <button type="submit"><i className="fa fa-search"></i></button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
