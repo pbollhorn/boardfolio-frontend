b82983bcfe1cac35b9c17dbffbf032adbc594812
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-10 14:27:55 +0100

    Broken search for now

--- src/pages/SearchGames.jsx
+++ src/pages/SearchGames.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import GameCardList from "../components/GameList.jsx";
+import "../styles/SearchGames.module.css";
+
 export default function SearchGames() {
+  const [gameList, setGameList] = useState([]);
+  const [statusMessage, setStatusMessage] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  async function fetchGameList() {
+    if (!searchTerm) {
+      setStatusMessage("Please enter a search term.");
+      return;
+    }
+
+    setStatusMessage("Searching...");
+    try {
+      const response = await fetch(
+        `https://movie.jcoder.dk/api/movies/search?title=${encodeURIComponent(searchTerm)}`
+      );
+      if (response.ok) {
+        const payload = await response.json();
+        setGameList(payload);
+        setStatusMessage(null);
+      } else {
+        setStatusMessage(`HTTP ${response.status}: ${response.statusText}`);
+      }
+    } catch (error) {
+      setStatusMessage(`An error occurred:\n${error.message}`);
+    }
+  }
+
   return (
-    <div class="container">
-      <h1>Search Games</h1>
+    <div className="container">
+      <div className="search-container">
+        <h1>Search For Games</h1>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="button" onClick={fetchGameList}>
+          <i className="fa fa-search"></i> Search
+        </button>
+      </div>
+
+      {/* Status message */}
+      {statusMessage && <p>{statusMessage}</p>}
+
+      {/* Display the fetched game list */}
+      {gameList.length > 0 && <GameCardList games={gameList} />}
     </div>
   );
 }
