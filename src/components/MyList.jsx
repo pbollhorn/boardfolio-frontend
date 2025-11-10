27d12e033fbdb15c8f205380c83b9aad34fffa48
Casper Alexander Gervig <gervig91@gmail.com>
2025-11-10 14:05:50 +0100

    Update MyList.jsx

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,36 +1,31 @@
 import GameList from "./GameList";
 import { useAuth } from "../context/AuthContext";
 import { fetchData } from "../util/fetchData";
-// import { fetchData } from "../util/fetchData"; TODO:import fetchData when it's made
+import { useEffect, useState } from "react";
 
 export default function MyList() {
   const { isLoggedIn, username } = useAuth(); //TODO: we need login for this to work
   const { userListOfGameLists, setUserListOfGameLists } = useState([]);
 
+  //   TODO: insert URL for fetching GameLists for a user here!
+  //   const URL = "INSERT URL HERE" + username;
+
+  // fetches the user's list of gameLists
   function getUserListOfGameLists(callback) {
     fetchData(URL, callback);
   }
 
   useEffect(() => {
     if (username) {
-      getUserListOfGameLists(setUserMovies);
+      getUserListOfGameLists(setUserListOfGameLists);
     }
   }, [username]);
 
-  //   TODO: insert URL for fetching GameLists for a user here!
-  //   const URL = "" + username;
-
-  //   TODO: use fetchData here!
-  //   function getGames(callback) {
-  //     fetchData(URL, callback);
-  //   }
-
-  //TODO: use effect, once other functions are made
-  //   useEffect(() => {
-  //     getGames((data) => {
-  //       setGames(data);
-  //     });
-  //   }, []);
+  useEffect(() => {
+    getUserListOfGameLists((data) => {
+      setUserListOfGameLists(data);
+    });
+  }, []);
 
   //   Early return if user is not logged in
   if (!isLoggedIn) {
@@ -46,9 +41,19 @@ export default function MyList() {
     <>
       <div class="container">
         <h1>Welcome {username}!</h1>
+        <br />
+        {/* TODO: add css to the table */}
+        <table>
+          <tbody>
+            {userListOfGameLists.map((gameList) => (
+              <tr key={gameList.listID}>
+                <td>{gameList.name}</td>
+                <td>{gameList.createdDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      {/* TODO: create component for showing a list
-      of the user's GameLists */}
     </>
   );
 }
