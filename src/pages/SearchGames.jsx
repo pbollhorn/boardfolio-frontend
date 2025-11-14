9b9409034e8f935afa566ab2320f96c0855e7684
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-14 17:13:57 +0100

    Search request now runs on every input in input box and on Enter

--- src/pages/SearchGames.jsx
+++ src/pages/SearchGames.jsx
@@ -1,26 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import GameCardList from "../components/GameList.jsx";
 
 export default function SearchGames() {
   const [gameList, setGameList] = useState([]);
   const [statusMessage, setStatusMessage] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
+  const searchTermRef = useRef(null);
 
-  async function fetchGameList() {
-    if (!searchTerm) {
-      setStatusMessage("Please enter a search term.");
-      return;
-    }
+  async function fetchGameList(event) {
+    event.preventDefault();
+
+    const searchTerm = searchTermRef.current.value;
+
+    // if (!searchTerm) {
+    //   setStatusMessage("Please enter a search term.");
+    //   return;
+    // }
 
     setStatusMessage("Searching...");
     try {
       const response = await fetch(
-        `https://movie.jcoder.dk/api/movies/search?title=${encodeURIComponent(searchTerm)}`
+        `https://movie.jcoder.dk/api/movies/search?title=${encodeURIComponent(
+          searchTerm
+        )}`
       );
       if (response.ok) {
         const payload = await response.json();
         setGameList(payload);
-        setStatusMessage(payload.length === 0 ? "No games found." : null);
+        setStatusMessage(null);
       } else {
         setStatusMessage(`HTTP ${response.status}: ${response.statusText}`);
       }
@@ -34,15 +41,20 @@ export default function SearchGames() {
       <h1>Search for Games</h1>
 
       <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search for games..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="button" onClick={fetchGameList}>
-          <span className="material-symbols-outlined">search</span>
-        </button>
+        <form onSubmit={fetchGameList}>
+          <input
+            type="search"
+            placeholder="Search for games..."
+            // value={searchTerm}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={fetchGameList}
+            ref={searchTermRef}
+          />
+          {/* <button type="submit"> */}
+          <button>
+            <span className="material-symbols-outlined">search</span>
+          </button>
+        </form>
       </div>
 
       {statusMessage ? (
