e38fe1f7a2e4cd59f4d91ef6a614f2419632159c
Gervig <gervig91@gmail.com>
2025-11-17 06:38:49 +0100

    Create Frontpage.jsx

---
+++ src/components/Frontpage.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "./LoginForm";
+
+export default function Frontpage() {
+  return (
+    <div>
+      <h2>Welcome to Boardfolio!</h2>
+      <LoginForm />
+    </div>
+  );
+}
