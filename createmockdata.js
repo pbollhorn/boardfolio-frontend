38c3cadb43a629952cb2980ebdd78fff3691c2de
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-20 08:19:17 +0100

    progression in createmockdata.js

--- createmockdata.js
+++ createmockdata.js
@@ -1,4 +1,4 @@
-import casual from 'casual';
+import casual from "casual";
 
 // TODO: Remember to put "json-server" under "devDependencies" instead of "dependencies"
 // TODO: Remember to say in README to run npm install
@@ -8,23 +8,29 @@ import casual from 'casual';
 // 1. Run this script using Node: node createmockdata.js
 // 2. Copy paste the output from terminal into mock-db.json
 
-
 // Create an object for config file
-var db = {books:[]};
+var db = { boardgames: [] };
 
-for(var i=101; i<=115; i++){
-    var book = {};
-  book.id = i;
+for (var i = 101; i <= 115; i++) {
+  var game = {};
+  game.id = i;
 
   // Create a random 1-6 word title
-  book.title = casual.words(casual.integer(1,6));
-  book.author = casual.first_name + ' ' + casual.last_name;
-  
-  // Randomly rate the book between 0 and 5
-  book.rating = Math.floor(Math.random()*100+1)/20;
+  game.title = casual.words(casual.integer(1, 6));
+  game.author = casual.first_name + " " + casual.last_name;
 
-  // Assign a publishing year between 1700 and 2016
-    book.year_published = casual.integer(1700,2016)
-    db.books.push(book);
+  // // Randomly rate the book between 0 and 5
+  // game.rating = Math.floor(Math.random()*100+1)/20;
+
+  // Assign a release year between 1900 and 2025
+  game.release_year = casual.integer(1900, 2025);
+
+
+  game.genre = ["Adventure","Animals","Dice","Fantasy","Horror"][Math.floor(Math.random()*5)];
+
+
+
+  db.boardgames.push(game);
 }
-console.log(JSON.stringify(db));
+// console.log(JSON.stringify(db));
+console.log(JSON.stringify(db, null, 2));
